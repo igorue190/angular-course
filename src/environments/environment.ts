fcd57e9d5ec8230dfde524b503535bea0cf72434
Nesterenko Igor <32173550+igorue190@users.noreply.github.com>
2021-10-13 17:01:06 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ import { Environment } from "./interface";
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyCtqGACjyeXIC-rc8jUeQCevoC7IgYEK1Q'
+  apiKey: '-------------------'
 };
 
 /*

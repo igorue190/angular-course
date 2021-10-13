4ad50a61ad8f1534ed0e8138ce8457d8f26cf462
Igor Nesterenko <inesterenko@madisonpg.com>
2021-10-13 16:49:59 +0300

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LayoutModule } from './shared/common/layout/layout.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RfRegistrationComponent } from './core/components/rf-registration/rf-registration.component';
-import { AppRoutingModule } from './app-routing/app-routing.module';
 import { StrenthPasswordComponent } from './shared/strenth-password/strenth-password.component';
 import { TdRegistrationComponent } from './core/components/td-registration/td-registration.component';
 import { HomeComponent } from './core/components/home/home.component';
 import { MustMatchDirective } from './shared/directives/must-match.directive';
 import { LoginComponent } from './core/components/login/login.component';
-import { LayoutModule } from './shared/common/layout/layout.module';
 import { AuthGuardGuard } from './app-routing/auth-guard.guard';
-import { HttpClientModule } from '@angular/common/http';
 import { FocusDirective } from './shared/directives/focus.directive';
-import { MatDialogModule } from '@angular/material/dialog'
+
 
 @NgModule({
   declarations: [

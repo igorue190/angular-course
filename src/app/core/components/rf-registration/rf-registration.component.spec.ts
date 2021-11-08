6be53591ec581452304c4d2374b96c52c54a8479
Igor Nesterenko <inesterenko@madisonpg.com>
2021-11-08 14:39:24 +0200

    q

--- src/app/core/components/rf-registration/rf-registration.component.spec.ts
+++ src/app/core/components/rf-registration/rf-registration.component.spec.ts
@@ -1,22 +1,31 @@
 import { FormBuilder} from '@angular/forms';
 import { of } from 'rxjs';
-import { FormUserData, FormGroupName } from 'src/app/model/mock-users';
-import { FormValidationService } from '../../services/form-validation.service';
-import { RegistrationServiceService } from '../../services/registration-service.service';
+import { FormUserData, FormGroupName, Users } from 'src/app/model/mock-users';
 
 import { RfRegistrationComponent } from './rf-registration.component';
 
 describe('RfRegistrationComponent', () => {
   
-  let component: RfRegistrationComponent
-  let regService : RegistrationServiceService
-  let dialogRefSpyObj = jasmine.createSpyObj({ afterClosed : of({}), close: null });
-  dialogRefSpyObj.componentInstance = { body: '' };
+  let component: RfRegistrationComponent;
+  let mockRegistrationService;
+  let mockFormValidationService;
+  let dialogRefSpyObj;
+  let USERS;
 
   beforeEach(() => {
-    regService = new RegistrationServiceService(null)
-    component = new RfRegistrationComponent( regService,
-                                            new FormValidationService(),
+    mockRegistrationService = jasmine.createSpyObj(['addUser', 'getUser']);
+    mockFormValidationService = jasmine.createSpyObj(['validateForm']);
+    dialogRefSpyObj = jasmine.createSpyObj(['open']);
+   USERS = [
+      {Id: 0, UserName: 'Igor', Email: 'igor@gmail.com', Gender: 'Genders', FirstName: 'Igor', LastName: 'Nesterenko', Password:'123456', Country: 'Ukraine', City: 'Kharkov', ZipCode: '67000'},
+      {Id: 1, UserName: 'Alex', Email: 'alex@gmail.com', Gender: 'Genders', FirstName: 'Alex', LastName: 'Pain', Password:'246810', Country: 'Ukraine', City: 'Kharkov', ZipCode: '67000'},
+      {Id: 2, UserName: 'Serg', Email: 'serg@gmail.com', Gender: 'Genders', FirstName: 'Serg', LastName: 'Train', Password:'1357911', Country: 'Ukraine', City: 'Kharkov', ZipCode: '67000'},
+      {Id: 3, UserName: 'Peter', Email: 'peter@gmail.com', Gender: 'Genders', FirstName: 'Peter', LastName: 'Game', Password:'654321', Country: 'Ukraine', City: 'Kharkov', ZipCode: '67000'},
+      {Id: 4, UserName: 'John', Email: 'john@gmail.com', Gender: 'Genders', FirstName: 'John', LastName: 'rain', Password:'111111', Country: 'Ukraine', City: 'Kharkov', ZipCode: '67000'}
+  ]
+
+    component = new RfRegistrationComponent( mockRegistrationService,
+                                            mockFormValidationService,
                                             new FormBuilder(),
                                             dialogRefSpyObj)
     component.registerForm = component.buildRegisterFormGroup()
@@ -25,7 +34,6 @@ describe('RfRegistrationComponent', () => {
   it('should create RfRegistrationComponent', () => {
     expect(component).toBeTruthy()
   })
-
   it('should create userName control', () => {
     expect(component.registerForm.contains(FormUserData.userName)).toBeTruthy()
   })
@@ -44,7 +52,6 @@ describe('RfRegistrationComponent', () => {
     userNameControl.setValue('0123456789!')
     expect(userNameControl.valid).toBeFalsy()
   })
-
   it('should create email control', () => {
     expect(component.registerForm.contains(FormUserData.email)).toBeTruthy()
   })
@@ -53,11 +60,9 @@ describe('RfRegistrationComponent', () => {
     emailControl.setValue('0123456789!')
     expect(emailControl.valid).toBeFalsy()
   })
-
   it('should create address array', () => {
     expect(component.registerForm.contains(FormGroupName.address)).toBeTruthy()
   })
-
   it('should create confirmPassword control', () => {
     expect(component.confirmPasswordControl).toBeTruthy()
   })
@@ -79,7 +84,6 @@ describe('RfRegistrationComponent', () => {
 
     expect(component.confirmPasswordControl.valid).toBeFalsy()
   })
-
   it('should create country control', () => {
     expect(component.countryListControl).toBeTruthy()
   })
@@ -90,16 +94,13 @@ describe('RfRegistrationComponent', () => {
     expect(component.zipcodeControl).toBeTruthy()
   })
 
-  it('should call addUser method', () => {
-    const spy = spyOn(regService, 'addUser').and.returnValue(true)
-    component.submit()
-    expect(spy).toHaveBeenCalled()
-  })
-  it('should call agetUser method', () => {
-    const spy1 = spyOn(regService, 'addUser').and.returnValue(true)
-    const spy2 = spyOn(regService, 'getUser').and.returnValue(component.users)
-    component.submit()
-    expect(spy1).toHaveBeenCalled()
-    expect(spy2).toHaveBeenCalled()
+  //  it('should call submit method', () => {
+  //    expect(component.submit()).toHaveBeenCalled()
+  //  })
+
+  it('should call getUser method', () => {
+    mockRegistrationService.getUser();
+    component.getUsers();
+    expect(mockRegistrationService.getUser()).toHaveBeenCalled()
   })
 });
